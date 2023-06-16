0767fe2544f81ad6b1abf585569ade55403e5876
adrianblazeusz <adrian.blazeusz@gmail.com>
2023-06-16 22:16:48 +0200

    Game

--- Lesson 1/geometry.py
+++ Lesson 1/geometry.py
@@ -1,19 +1,24 @@
 class Point:
-
+ 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+ 
     def falls_in_rectangle(self, rectangle):
-        if rectangle.lowleft.x < self.x < rectangle.upright.x \
-        and rectangle.lowleft.y < self.y < rectangle.upright.y:
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
             return True
         else:
             return False
-        
+ 
+ 
 class Rectangle:
-
-    def __init__(self, lowleft, upright):
-        self.lowleft = lowleft
-        self.upright = upright
+ 
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+ 
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
 
--- Lesson 1/grometru_game.py
+++ Lesson 1/grometru_game.py
@@ -1,17 +1,21 @@
 from geometry import Point, Rectangle
 from random import randint
 
-rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
+# Create rectangle object
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)), 
                       Point(randint(10, 19), randint(10, 19)))
-
-print("Rectangle Coordinates: X1:",
-      rectangle.lowleft.x, ", Y1:",
-      rectangle.lowleft.y, "and X2:",
-      rectangle.upright.x, ", Y2:",
-      rectangle.upright.y)
-
-user_point = Point(float(input("Guess X: ")),
-                   float(input("Guess Y: ")))
-
-print("Your point was inside rectangle: ",
-      user_point.falls_in_rectangle(rectangle))
+ 
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+ 
+# Get point and area from user
+user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+ 
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
