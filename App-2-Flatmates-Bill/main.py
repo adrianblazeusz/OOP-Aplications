f94a13ccf3777dc302027abc513998715ef4d7e2
Adrian Błazeusz <adrianblazeusz@gmail.com>
2023-06-18 01:40:37 +0200

    1

---
+++ App-2-Flatmates-Bill/main.py
@@ -0,0 +1,41 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as
+    total amount and period of the bill.
+    """
+
+    def __int__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Create a flatmate person who lives in the flat
+    and pays a share of the bill.
+    """
+
+    def __int__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount / 2
+
+class PdfReport:
+    """
+    Creates a Pdf file that contains data about
+    the flatmates such as their name, their due amount
+    and the period of the bill.
+    """
+
+    def __int__(self, filename):
+        self.filename = filename
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+the_bill = Bill(amount = 120, period = "March 2021")
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate(name="Marry", days_in_house=25)
+
+print(john.pays(bill=the_bill))
