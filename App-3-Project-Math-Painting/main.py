c2d25c2e728cb43f8eba03079e434f89db12a328
Adrian Błazeusz <adrianblazeusz@gmail.com>
2023-06-19 20:57:39 +0200

    App-3

---
+++ App-3-Project-Math-Painting/main.py
@@ -0,0 +1,29 @@
+class Canvas:
+
+    def __init__(self, a, b, color):
+        self.color = color
+        self.a = a
+        self.b = b
+
+class Rectangle:
+
+    def __init__(self, x, y, a, b, color):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        self.color = color
+
+    def draw(self, canvas):
+        pass
+
+class Square:
+
+    def __init__(self, x, y, a, color):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.a = a
+
+    def draw(self, canvas):
+        pass
