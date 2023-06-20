402d1f254b8d5ef1fa8954a52713a981608c76e3
Adrian Błazeusz <adrianblazeusz@gmail.com>
2023-06-20 20:24:02 +0200

    Flask

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -1,14 +1,30 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 class HomePage(MethodView):
-    pass
+
+    def get(self):
+        return render_template("index.html")
+
 
 class BillFormPage(MethodView):
-    pass
+    def get(self):
+        return "I am the bill form page!"
+
 
 class ResultsPage(MethodView):
     pass
 
+
 class BillForm(Form):
     pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_from_page'))
+
+app.run(debug=True)
